f1b604df315d03a824590a24d0587443961fcd65
Arnav Sawant <139108926+arnavsawant9@users.noreply.github.com>
2025-06-04 11:28:15 +0530

    Time: 173 ms (5.07%), Space: 62.5 MB (5.06%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,17 +1,64 @@
 import java.util.Stack;
+class Pair{
+    int price;
+    int span;
+    Pair(int price, int span){
+        this.price = price;
+        this.span = span;
+    }
+}
 
 class Solution {
     public int largestRectangleArea(int[] heights) {
+        Stack<Pair> stack1 = new Stack<>();
         List<Integer> list1 = new ArrayList<>();
-        List<Integer> list2 = new ArrayList<>();
-        Stack<Integer> stack1 = new Stack<>();
-        Stack<Integer> stack2 = new Stack<>();
-
-        // calculate nearest smallest to left
+        // calculating NSL here!
         for(int i=0;i<heights.length;i++){
             if(stack1.size() == 0){
-                lis1.add(-1);
-            } else if(stack.size() > 0 && stack.peek() )
+                list1.add(-1);
+            } else if (stack1.size() > 0 && stack1.peek().price < heights[i]){
+                list1.add(stack1.peek().span);
+            } else if (stack1.size() > 0 && stack1.peek().price >= heights[i]){
+                while(stack1.size() > 0 && stack1.peek().price >= heights[i]){
+                    stack1.pop();
+                }
+                if(stack1.size() == 0){
+                    list1.add(-1);
+                } else {
+                    list1.add(stack1.peek().span);
+                }
+            }
+            stack1.push(new Pair(heights[i], i));
         }
+
+
+        Stack<Pair> stack2 = new Stack<>();
+        List<Integer> list2 = new ArrayList<>();
+        // calculating NSR here!
+        for(int i=heights.length - 1;i>=0;i--){
+            if(stack2.size() == 0){
+                list2.add(heights.length);
+            } else if (stack2.size() > 0 && stack2.peek().price < heights[i]){
+                list2.add(stack2.peek().span);
+            } else if (stack2.size() > 0 && stack2.peek().price >= heights[i]){
+                while(stack2.size() > 0 && stack2.peek().price >= heights[i]){
+                    stack2.pop();
+                }
+                if(stack2.size() == 0){
+                    list2.add(heights.length);
+                } else {
+                    list2.add(stack2.peek().span);
+                }
+            }
+            stack2.push(new Pair(heights[i], i));
+        }
+        Collections.reverse(list2);
+
+        int max = Integer.MIN_VALUE;
+        for(int i =0;i<heights.length;i++){
+            max = Math.max(max, (Math.abs(list1.get(i) - list2.get(i)) - 1)*heights[i]);
+        }
+
+        return max;
     }
 }
