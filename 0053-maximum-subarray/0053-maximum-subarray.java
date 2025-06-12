5d91bedfe9fc57e6e4debb1e60039159a2c2dfec
Arnav Sawant <139108926+arnavsawant9@users.noreply.github.com>
2025-06-12 19:45:35 +0530

    Time: 1 ms (99.6%), Space: 56.8 MB (89.22%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -4,9 +4,8 @@ class Solution {
         int maxSum = Integer.MIN_VALUE;
         for(int i=0;i<nums.length;i++){
             sum += nums[i];
-            if(sum > maxSum){
-                maxSum = sum;
-            }
+            
+            maxSum = Math.max(sum, maxSum);
 
             if(sum <0){
                 sum = 0;
