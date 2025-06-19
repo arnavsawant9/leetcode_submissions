a6946c013fc07aedbd9cd6a910c95fddc102ea45
Arnav Sawant <139108926+arnavsawant9@users.noreply.github.com>
2025-06-20 00:22:22 +0530

    Time: 0 ms (100%), Space: 43.3 MB (20.38%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,19 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double ans = 1.0;
+        long num = n;
+        if(num < 0) num = num * -1;
+        while(num > 0){
+            if(num % 2 == 0){
+                x = x * x;
+                num = num/2;
+            } else {
+                ans = ans * x;
+                num = num - 1;
+            }
+        }
+
+        if(n < 0) return (double) 1.0/ (double)(ans);
+        return ans;
+    }
+}
