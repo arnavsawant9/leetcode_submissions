a3a66380941fa604f0330044a49f2eedf2dc4ab8
Arnav Sawant <139108926+arnavsawant9@users.noreply.github.com>
2025-06-27 23:58:54 +0530

    Time: 14 ms (12.74%), Space: 45.8 MB (81.89%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(hm.containsKey(nums[i])){
+                int temp = hm.get(nums[i]);
+                hm.put(nums[i], temp+1);
+            } else {
+                hm.put(nums[i], 1);
+            }
+        }
+
+        for(int i=0;i<nums.length;i++){
+            if(hm.get(nums[i]) == 1){
+                return nums[i];
+            }
+        }
+
+        return -1;
+    }
+}
