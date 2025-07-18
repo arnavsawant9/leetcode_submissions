8da943c92c5a73e3a3e3637ecc321a37b3fec500
Arnav Sawant <139108926+arnavsawant9@users.noreply.github.com>
2025-07-18 14:01:01 +0530

    Time: 18 ms (6.41%), Space: 58.3 MB (79.96%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -1,25 +1,19 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ListNode slow = head, fast = head, prev, temp;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
+        if(head.next == null) return true;
+        Stack<Integer> stack = new Stack<>();
+        ListNode temp = head;
+        while(temp != null){
+            stack.push(temp.val);
+            temp = temp.next;
         }
-        prev = slow;
-        slow = slow.next;
-        prev.next = null;
-        while (slow != null) {
-            temp = slow.next;
-            slow.next = prev;
-            prev = slow;
-            slow = temp;
-        }
-        fast = head;
-        slow = prev;
-        while (slow != null) {
-            if (fast.val != slow.val) return false;
-            fast = fast.next;
-            slow = slow.next;
+
+        // now we will pop the stack in the reverse order
+        ListNode curr = head;
+        while(stack.size() > 0){
+            if(stack.peek() != curr.val) return false;
+            stack.pop();
+            curr = curr.next;
         }
         return true;
     }
